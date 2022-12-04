f67a2267bd73370bfb782df48da4758c39dc55e5
OSandh <oskar@binarybrains.se>
2022-12-04 15:29:13 +0100

    part 2

--- day2/d2.js
+++ day2/d2.js
@@ -8,14 +8,23 @@ const cheatTheElfs = (cheatSheet) => {
 	for (const round of cheatSheet.split('\n')) {
 		moves = round.split(' ');
 		const elfMove = moves[0];
-		const myMove = moves[1].replace('\r', '');
-
+		const myMove = getMyMove(elfMove, moves[1].replace('\r', ''));
 		// 0 if you lost, 3 if the round was a draw, and 6 if you won
-		myTotalScore += playRound(elfMove, myMove) * 3 + getMoveScore(myMove);
+		const roundScore = playRound(elfMove, myMove);
+		myTotalScore += roundScore * 3 + getMoveScore(myMove);
 	}
 	console.log('My total score:', myTotalScore, 'forsenCD');
 };
 
+// X == lose, Y == draw, Z == win
+const getMyMove = (elfMove, rountOutcome) => {
+	const elfMoveCode = elfMove.charCodeAt(0);
+	let returnCode = elfMoveCode;
+	if (rountOutcome == 'X') returnCode = elfMoveCode + (['B', 'C'].includes(elfMove) ? -1 : 2);
+	else if (rountOutcome == 'Z') returnCode = elfMoveCode + (['A', 'B'].includes(elfMove) ? 1 : -2);
+
+	return String.fromCharCode(returnCode + 23);
+};
 // oponent:	A for Rock, B for Paper, and C for Scissors
 // me: 		X for Rock, Y for Paper, and Z for Scissors
 const playRound = (elfMove, myMove) => {
