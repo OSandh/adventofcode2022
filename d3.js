62dff947b807cbc1bc06d9aab26814976f191b09
OSandh <oskar@binarybrains.se>
2022-12-04 20:22:00 +0100

    day 3 part 2

--- d3.js
+++ d3.js
@@ -2,31 +2,46 @@ const fs = require('fs');
 fs.readFile(__dirname + '/day3/input.txt', 'utf-8', (err, data) => {
 	// fix for windows scrubs
 	const lb = data.includes('\r') ? '\r\n' : '\n';
-
-	fixRucksacks(data.split(lb));
+	partOne(data.split(lb));
+	partTwo(data.split(lb));
 });
 
-const fixRucksacks = (rucksacks) => {
-	const prioSum = getRucksackPrioSum(rucksacks);
-	console.log('Sum of type priorites:', prioSum);
-};
-
-const getRucksackPrioSum = (rucksacks) => {
-	const prioDict = { ...getPrioDict('a', 'z'), ...getPrioDict('A', 'Z', 26) };
+const partOne = (rucksacks) => {
 	let prioSum = 0;
 	for (const rucksack of rucksacks) {
-		const { c1, c2 } = getCompartments(rucksack);
-		const typeInBoth = c1.split('').find((char) => c2.includes(char));
-		if (typeInBoth) prioSum += prioDict[typeInBoth];
+		const compartments = [rucksack.substring(0, rucksack.length / 2), rucksack.substring(rucksack.length / 2)];
+		prioSum += getRucksackPrioSum(compartments);
 	}
-	return prioSum;
+	console.log('Sum of type priorites (part1):', prioSum);
+};
+const partTwo = (rucksacks) => {
+	let prioSum = 0;
+	let groupSacks = []; // o_o
+	for (const [i, rucksack] of Object.entries(rucksacks)) {
+		const gmod = i + 1;
+		groupSacks.push(rucksack);
+		if (gmod % 3 === 0 && gmod % 1 === 0) {
+			// got group
+			prioSum += getRucksackPrioSum(groupSacks);
+			groupSacks = [];
+		}
+	}
+	console.log('Sum of type priorites (part2):', prioSum);
 };
 
-const getCompartments = (rucksack) => {
-	return {
-		c1: rucksack.substring(0, rucksack.length / 2),
-		c2: rucksack.substring(rucksack.length / 2),
-	};
+const getRucksackPrioSum = (cmps) => {
+	const prioDict = { ...getPrioDict('a', 'z'), ...getPrioDict('A', 'Z', 26) };
+	let prioSum = 0;
+	const typeInBoth = cmps[0].split('').find((char) => {
+		let i = 0;
+		const valid = [];
+		while (i < cmps.length - 1) {
+			valid.push(cmps[++i].includes(char));
+		}
+		return !valid.includes(false);
+	});
+	if (typeInBoth) prioSum += prioDict[typeInBoth];
+	return prioSum;
 };
 
 const getPrioDict = (start, end, offset = 0) => {
