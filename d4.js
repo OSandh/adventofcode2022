211a6c894a117679ef1f2ea879839fb36dff95a1
OSandh <oskar@binarybrains.se>
2022-12-05 01:25:56 +0100

    part 2 done

--- d4.js
+++ d4.js
@@ -1,27 +1,34 @@
-// assigned sections with unique ID number
-// assignmenets overlap
-// pair up, list of section assignments for each pair
-// 2-4,6-8
-// 2-3,4-5
-// 5-7,7-9
-// 2-8,3-7
-// 6-6,4-6
-// 2-6,4-8
 const fs = require('fs');
-const data = fs.readFileSync(__dirname + '/day4/4-input.txt', 'utf-8').split`\n`;
+const data = fs.readFileSync(__dirname + '/day4/4-input.txt', 'utf-8').split`\n`.map((pair) =>
+	pair.split`,`.reduce((raw, assignment) => {
+		raw.push(assignment.split`-`.map((n) => +n));
+		return raw;
+	}, [])
+);
 
 const partOne = (data) => {
 	let overlaps = 0;
-	for (const pair of data) {
-		const rawPair = pair.split`,`.reduce((raw, assignment) => {
-			raw.push(assignment.split`-`.map((n) => +n));
-			return raw;
-		}, []);
-
-		const firstInSecond = rawPair[0][0] >= rawPair[1][0] && rawPair[0][1] <= rawPair[1][1];
-		const secondInFirst = rawPair[1][0] >= rawPair[0][0] && rawPair[1][1] <= rawPair[0][1];
+	for (const rP of data) {
+		const firstInSecond = rP[0][0] >= rP[1][0] && rP[0][1] <= rP[1][1];
+		const secondInFirst = rP[1][0] >= rP[0][0] && rP[1][1] <= rP[0][1];
 		if (firstInSecond || secondInFirst) overlaps++;
 	}
-	console.log('overlaps', overlaps);
+	console.log('overlaps part1:', overlaps);
 };
+
+const partTwo = (data) => {
+	let overlaps = 0;
+	for (const rP of data) {
+		const firstElfS = rP[0][0] >= rP[1][0] && rP[0][0] <= rP[1][1];
+		const firstElfE = rP[0][1] >= rP[1][0] && rP[0][1] <= rP[1][1];
+
+		const secElfS = rP[1][0] >= rP[0][0] && rP[1][0] <= rP[0][1];
+		const secElfE = rP[1][1] >= rP[0][0] && rP[1][1] <= rP[0][1];
+
+		if (firstElfS || firstElfE || secElfS || secElfE) overlaps++;
+	}
+	console.log('overlaps part2:', overlaps);
+};
+
 partOne(data);
+partTwo(data);
