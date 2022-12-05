5ec3bf5031803088adec32586943582b026d2c70
OSandh <oskar@binarybrains.se>
2022-12-05 01:31:19 +0100

    readability

--- d4.js
+++ d4.js
@@ -8,9 +8,13 @@ const data = fs.readFileSync(__dirname + '/day4/4-input.txt', 'utf-8').split`\n`
 
 const partOne = (data) => {
 	let overlaps = 0;
-	for (const rP of data) {
-		const firstInSecond = rP[0][0] >= rP[1][0] && rP[0][1] <= rP[1][1];
-		const secondInFirst = rP[1][0] >= rP[0][0] && rP[1][1] <= rP[0][1];
+	for (const pair of data) {
+		const elfOne = pair[0];
+		const elfTwo = pair[1];
+
+		const firstInSecond = elfOne[0] >= elfTwo[0] && elfOne[1] <= elfTwo[1];
+		const secondInFirst = elfTwo[0] >= elfOne[0] && elfTwo[1] <= elfOne[1];
+
 		if (firstInSecond || secondInFirst) overlaps++;
 	}
 	console.log('overlaps part1:', overlaps);
@@ -18,12 +22,15 @@ const partOne = (data) => {
 
 const partTwo = (data) => {
 	let overlaps = 0;
-	for (const rP of data) {
-		const firstElfS = rP[0][0] >= rP[1][0] && rP[0][0] <= rP[1][1];
-		const firstElfE = rP[0][1] >= rP[1][0] && rP[0][1] <= rP[1][1];
+	for (const pair of data) {
+		const elfOne = pair[0];
+		const elfTwo = pair[1];
 
-		const secElfS = rP[1][0] >= rP[0][0] && rP[1][0] <= rP[0][1];
-		const secElfE = rP[1][1] >= rP[0][0] && rP[1][1] <= rP[0][1];
+		const firstElfS = elfOne[0] >= elfTwo[0] && elfOne[0] <= elfTwo[1];
+		const firstElfE = elfOne[1] >= elfTwo[0] && elfOne[1] <= elfTwo[1];
+
+		const secElfS = elfTwo[0] >= elfOne[0] && elfTwo[0] <= elfOne[1];
+		const secElfE = elfTwo[1] >= elfOne[0] && elfTwo[1] <= elfOne[1];
 
 		if (firstElfS || firstElfE || secElfS || secElfE) overlaps++;
 	}
