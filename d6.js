6b4bb6cdcbcbb5e03b07b2b181126275a7ce32eb
OSandh <oskar@binarybrains.se>
2022-12-06 19:39:04 +0100

    day 6 part 2

--- d6.js
+++ d6.js
@@ -1,21 +1,21 @@
 const data = require('fs').readFileSync('./inputs/i6.txt', 'utf-8').split`\n`.map((r) => r.split``);
-for (const msg of data) {
-	let i = 0;
-	while (i < msg.length) {
-		let found = true;
-		let grp = msg.slice(i, i + 4);
-		for (let c of grp) {
-			let other = grp.filter((o) => o === c);
-			if (other.length > 1 && grp.length === 4) {
-				found = false;
-				break;
-			}
-		}
 
-		if (found) {
-			console.log('MARKER FOUND', grp.join``, i + 4);
-			break;
+const findMarker = (markerLength = 4) => {
+	for (const msg of data) {
+		let i = -1;
+		while (++i < msg.length) {
+			let found = true;
+			let grp = msg.slice(i, i + markerLength);
+			for (let c of grp) {
+				let other = grp.filter((o) => o === c);
+				if (other.length > 1 && grp.length === markerLength) {
+					found = false;
+					break;
+				}
+			}
+			if (found) return i + markerLength;
 		}
-		i++;
 	}
-}
+};
+console.log('start-of-packet:', findMarker());
+console.log('start-of-message:', findMarker(14));
