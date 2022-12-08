78fd2d724d41a8e5c9c37438140ad2f30422df14
OSandh <oskar@binarybrains.se>
2022-12-08 01:14:20 +0100

    refactor

--- d7.js
+++ d7.js
@@ -1,39 +1,33 @@
 const data = require('fs').readFileSync('./inputs/i7.txt', 'utf-8').split`\n`;
 
-let inDir = '';
-const filesystem = data.reduce(
-	(fs, line) => {
-		const ls = line.includes('$ ls');
-		if (line[0] === '$' && !ls) {
-			fs.cmd.push(line.slice(2, line.length));
-			if (line.slice(2, 4).includes('cd') && line.slice(5, line.length) !== '..') {
-				inDir += (inDir !== '' ? '>' : '') + line.slice(5, line.length);
-				fs.dir[inDir] = { name: inDir, subdirs: [], files: [], local_size: 0, total_size: 0 };
-			} else {
-				inDir = inDir.includes('>') ? inDir.slice(0, inDir.lastIndexOf('>')) : '';
-			}
-		} else if (inDir && !ls) {
-			if (line.slice(0, 3).includes('dir')) {
-				fs.dir[inDir].subdirs.push(inDir + '>' + line.slice(4, line.length));
-			} else {
-				let [filesize, name] = line.split` `;
-				fs.dir[inDir].files.push({ size: +filesize, name: name });
-				fs.dir[inDir].local_size += +filesize;
-				let predir = '';
-				let dirs = inDir.split`>`;
-				for (let d of dirs) {
-					fs.dir[predir + d].total_size += +filesize;
-					predir += d + '>';
-				}
+let dir = '';
+const filesystem = data.reduce((fs, line) => {
+	const ls = line.includes('$ ls');
+	if (line[0] === '$' && !ls) {
+		if (line.slice(2, 4).includes('cd') && line.slice(5, line.length) !== '..') {
+			dir += (dir !== '' ? '>' : '') + line.slice(5, line.length);
+			fs[dir] = { name: dir, subdirs: [], files: [], local_size: 0, total_size: 0 };
+		} else dir = dir.includes('>') ? dir.slice(0, dir.lastIndexOf('>')) : '';
+	} else if (dir && !ls) {
+		if (line.slice(0, 3).includes('dir')) {
+			fs[dir].subdirs.push(dir + '>' + line.slice(4, line.length));
+		} else {
+			let [filesize, name] = line.split` `;
+			fs[dir].files.push({ size: +filesize, name: name });
+			fs[dir].local_size += +filesize;
+			let predir = '';
+			let dirs = dir.split`>`;
+			for (let d of dirs) {
+				fs[predir + d].total_size += +filesize;
+				predir += d + '>';
 			}
 		}
-		return fs;
-	},
-	{ cmd: [], dir: {} }
-);
-//console.log(filesystem.dir['/']);
+	}
+	return fs;
+}, {});
+
 const partOne = () => {
-	let totalUnderLimit = Object.values(filesystem.dir)
+	let totalUnderLimit = Object.values(filesystem)
 		.filter((dir) => dir.total_size <= 100000)
 		.reduce((total, dir) => {
 			total += dir.total_size;
@@ -43,9 +37,8 @@ const partOne = () => {
 };
 
 const partTwo = () => {
-	let deleteCandidate = filesystem.dir['/'].total_size;
-	const spaceNeeded = 30000000 - (70000000 - deleteCandidate);
-	const dirToDelete = Object.values(filesystem.dir)
+	const spaceNeeded = 30000000 - (70000000 - filesystem['/'].total_size);
+	const dirToDelete = Object.values(filesystem)
 		.filter((d) => d.total_size > spaceNeeded)
 		.sort((a, b) => a.total_size - b.total_size)[0];
 	console.log('\ndir to delete (part2):');
