28cfd197567749e8302ac7b5d00a2d8de9719af4
OSandh <oskar@binarybrains.se>
2022-12-08 22:40:26 +0100

    day 8 part 2

--- d8.js
+++ d8.js
@@ -8,45 +8,65 @@ const grid = require('fs').readFileSync('inputs/i8.txt', 'utf-8').split`\n`.redu
 	return treegrid;
 }, []);
 
-const north = (tree, ri, ci) => {
+const north = (tree, ri, ci, scenic = false) => {
+	let score = 0;
 	let i = ri - 1;
 	while (i >= 0) {
-		if (tree <= grid[i--][ci]) return false;
+		let friend = grid[i--][ci];
+		score++;
+		if (tree <= friend) return scenic ? score : false;
 	}
-	return true;
+	return scenic ? score : true;
 };
-const south = (tree, ri, ci) => {
+const south = (tree, ri, ci, scenic = false) => {
+	let score = 0;
 	let i = ri + 1;
-	while (i < grid.length) if (tree <= grid[i++][ci]) return false;
-	return true;
+	while (i < grid.length) {
+		let friend = grid[i++][ci];
+		score++;
+		if (tree <= friend) return scenic ? score : false;
+	}
+	return scenic ? score : true;
 };
-const west = (tree, ri, ci) => {
+const west = (tree, ri, ci, scenic = false) => {
+	let score = 0;
 	let i = ci - 1;
-	while (i >= 0) if (tree <= grid[ri][i--]) return false;
-	return true;
+	while (i >= 0) {
+		let friend = grid[ri][i--];
+		score++;
+		if (tree <= friend) return scenic ? score : false;
+	}
+	return scenic ? score : true;
 };
-const east = (tree, ri, ci) => {
+const east = (tree, ri, ci, scenic = false) => {
+	let score = 0;
 	let i = ci + 1;
 	while (i < grid.length) {
-		if (tree <= grid[ri][i++]) return false;
+		let friend = grid[ri][i++];
+		score++;
+		if (tree <= friend) return scenic ? score : false;
 	}
-	return true;
+	return scenic ? score : true;
 };
 
 let visible = grid.length * 4 - 4;
-const countVisible = (row, ri) => {
-	for (const [ci, tree] of Object.entries(row)) {
-		if (ci > 0 && ci < row.length - 1) {
-			if (north(tree, +ri, +ci) || south(tree, +ri, +ci) || west(tree, +ri, +ci) || east(tree, +ri, +ci)) {
-				visible++;
+const countVisible = (tree, ri, ci) => {
+	if (north(tree, ri, ci) || south(tree, ri, ci) || west(tree, ri, ci) || east(tree, ri, ci)) visible++;
+};
+const getScenicScore = (tree, ri, ci) => {
+	return north(tree, ri, ci, true) * south(tree, ri, ci, true) * west(tree, ri, ci, true) * east(tree, ri, ci, true);
+};
+let scenicScores = [];
+for (const [ri, row] of Object.entries(grid)) {
+	if (ri > 0 && ri < row.length - 1) {
+		for (const [ci, tree] of Object.entries(row)) {
+			if (ci > 0 && ci < row.length - 1) {
+				countVisible(tree, +ri, +ci);
+				scenicScores.push({ tree: `${ri},${ci}`, score: getScenicScore(tree, +ri, +ci) });
 			}
 		}
 	}
-};
-
-for (const [ri, row] of Object.entries(grid)) {
-	if (ri > 0 && ri < row.length - 1) {
-		countVisible(row, ri);
-	}
 }
 console.log(visible, 'visible trees');
+const { tree, score } = scenicScores.sort((a, b) => b.score - a.score)[0];
+console.log(tree, 'is the most scenic tree with a score of', score);
